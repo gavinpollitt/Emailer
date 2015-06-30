b5bdb105712b4e65a93617d46af3d914000b4f17
gavin <gavin@127.0.53.53>
2015-06-30 14:10:16 +0100

    GITted up

---
+++ Utilities/src/main/java/uk/gav/utilities/Environment.java
@@ -0,0 +1,69 @@
+package uk.gav.utilities;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Environment {
+
+	private final static String PROP_LOC = "/opt/app/ufa/ufacit1/live/ufadomain/properties/";
+	private final static String JMSENVSOURCE = "jms.properties";
+	private final static String CONTEXTENVSOURCE = "connection.properties";
+	private final static String EMAILENVSOURCE = "email.properties";
+	private final static Properties jmsEnv;
+	private final static Properties contextEnv;
+	private final static Properties emailEnv;
+	
+	static {
+		try {
+			jmsEnv = loadEnvironment(JMSENVSOURCE);
+			contextEnv = loadEnvironment(CONTEXTENVSOURCE);
+			emailEnv = loadEnvironment(EMAILENVSOURCE);
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Cannot load environment variables::" + e);
+		}
+	}
+	
+	private final static Properties loadEnvironment(String envSource) throws Exception {
+		Properties prop = new Properties();
+		InputStream iStream = null;
+		
+		System.out.println("Attempting to load properties for::"+ envSource);
+		//Try and load from filesystem first
+		try {
+			iStream = new FileInputStream(PROP_LOC + envSource);
+			prop.load(iStream);
+			System.out.println(envSource + " loaded from filesystem");
+		}
+		catch (Exception e) {
+				System.out.println("GAVVYYYYY::::" + e); 
+		}
+			
+		//Otherwise from classpath
+		if (iStream == null) {
+			iStream = Environment.class.getClassLoader()
+					.getResourceAsStream("META-INF/" + envSource);
+			prop.load(iStream);
+			System.out.println(envSource + " loaded from classpath");
+		}
+		
+		return prop;
+	}
+
+
+	public static Properties getContextEnv() {
+		return contextEnv;
+	}
+
+
+	public static Properties getJmsEnv() {
+		return jmsEnv;
+	}
+
+	public static Properties getEmailEnv() {
+		return emailEnv;
+	}
+
+	
+}
